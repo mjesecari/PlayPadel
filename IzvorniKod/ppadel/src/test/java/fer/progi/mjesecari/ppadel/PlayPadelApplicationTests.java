7eb2ce9a94994f42df6ec5e7c7f6b3b1b0a6a724
unknown <bz54953@fer.hr>
2024-11-05 20:14:32 +0100

    Added OAuth2 loggin using Gmail and set target URL when logged in

---
+++ IzvorniKod/ppadel/src/test/java/fer/progi/mjesecari/ppadel/PlayPadelApplicationTests.java
@@ -0,0 +1,13 @@
+package fer.progi.mjesecari.ppadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayPadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ppadelspring/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author borna
+ *
+ */
+module ppadelspring {
+}

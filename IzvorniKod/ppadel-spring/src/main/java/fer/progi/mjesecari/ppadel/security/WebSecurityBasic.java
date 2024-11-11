b58d5c71a719c8b0e80e733fd24ef96f79a3eddb
BornaZelic <borna.zelic@fer.unizg.hr>
2024-11-11 16:39:31 +0100

    Update WebSecurityBasic.java
    Transfered changes from ppadel to ppadel-spring

--- IzvorniKod/ppadel-spring/src/main/java/fer/progi/mjesecari/ppadel/security/WebSecurityBasic.java
+++ IzvorniKod/ppadel-spring/src/main/java/fer/progi/mjesecari/ppadel/security/WebSecurityBasic.java
@@ -1,87 +1,68 @@
 package fer.progi.mjesecari.ppadel.security;
 
 
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableMethodSecurity(securedEnabled = true, prePostEnabled = false)
+@EnableWebSecurity
 public class WebSecurityBasic {
 
     @Value("${progi.fronted.url}")
     private String frontendUrl;
 
-    @Bean
-    @Profile("basic-security")
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable);
-        return http.build();
-    }
+    @Autowired
+    private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
-    // login: localhost:8080/oauth2/authorization/google
     @Bean
     @Profile("oauth-security")
-    public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> {
-                auth.anyRequest().authenticated();
-            })
-            .oauth2Login(oauth2 -> {
-                oauth2
-                    .userInfoEndpoint(
-                        userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
-                    .successHandler(
-                        (request, response, authentication) -> {
-                            response.sendRedirect(frontendUrl);
-                        });
-            })
-            .exceptionHandling(handling -> handling.authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
-            .build();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(CorsConfigurationSource()))
+                .authorizeHttpRequests(auth -> {
+            auth.anyRequest().authenticated();
+        }).oauth2Login(oauth2 -> {
+            oauth2.userInfoEndpoint(
+                            userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
+                    .successHandler(oAuth2LoginSuccessHandler);
+        }).headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .build();
     }
 
-
-    // @Bean
-    // @Profile({ "basic-security", "form-security" })
-    // @Order(Ordered.HIGHEST_PRECEDENCE)
-    // public SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
-    //     http.securityMatcher(PathRequest.toH2Console());
-    //     http.csrf(AbstractHttpConfigurer::disable);
-    //     http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
-    //     return http.build();
-    // }
+    @Bean
+    CorsConfigurationSource CorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return urlBasedCorsConfigurationSource;
+    }
 
     private GrantedAuthoritiesMapper authorityMapper() {
         final SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-
         authorityMapper.setDefaultAuthority("ROLE_ADMIN");
-
         return authorityMapper;
     }
 

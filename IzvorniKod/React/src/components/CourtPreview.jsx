7859ca8f18c25b318a77b869afd2faae0c2814f5
LorenaIvanisevic <lorena.ivanisevic@fer.unizg.hr>
2024-12-12 11:27:07 +0100

    court preview working, but ugly css

--- IzvorniKod/React/src/components/CourtPreview.jsx
+++ IzvorniKod/React/src/components/CourtPreview.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "@/components/Navbar";
 import {
 	Card,
@@ -8,56 +8,99 @@ import {
 	CardContent,
 	CardFooter,
 } from "./ui/card";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@headlessui/react";
-import { HeartIcon } from "lucide-react";
+import axios from "axios";
 
 export default function CourtPreview() {
-	// const userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+	const [courts, setCourts] = useState([]);
 
 	const [userInfo, setUserInfo] = useState(() => {
 		const savedUserInfo = sessionStorage.getItem("userInfo");
 		return savedUserInfo ? JSON.parse(savedUserInfo) : null;
 	});
 
-	console.log(userInfo);
-	const courts = [
-		{
-			name: " naziv terena ",
-			type: "vanjski ",
-			location: "adress",
-			contactinfo: "email vlasnika terena",
-		},
-		{
-			name: " naziv terena ",
-			type: "vanjski ",
-			location: "adress",
-			contactinfo: "email vlasnika terena",
-		},
-		{
-			name: " naziv terena ",
-			type: "vanjski ",
-			location: "adress",
-			contactinfo: "email vlasnika terena",
-		},
-		{
-			name: " naziv terena ",
-			type: "vanjski ",
-			location: "adress",
-			contactinfo: "email vlasnika terena",
-		},
-		{
-			name: " naziv terena ",
-			type: "vanjski ",
-			location: "adress",
-			contactinfo: "email vlasnika terena",
-		},
-		{
-			name: " naziv terena ",
-			type: "vanjski ",
-			location: "adress",
-			contactinfo: "email vlasnika terena",
-		},
-	];
+	const [openRes, setOpenRes] = useState(false);
+	const [openTerenInp, setOpenTerenInp] = useState(false);
+
+	const [form, setForm] = useState({
+		naziv: "",
+		tip: "",
+		vlasnikTerenaId: userInfo.id,
+	});
+
+	function fetchCourts() {
+		axios
+			.get("/api/tereni/")
+			.then((res) => {
+				setCourts(res.data);
+			})
+			.catch((error) => console.log(error));
+	}
+
+	useEffect(() => {
+		fetchCourts();
+	}, [openRes]);
+
+	const onChange = (event) => {
+		const { name, value } = event.target;
+		setForm((oldForm) => ({ ...oldForm, [name]: value }));
+		console.log(form);
+	};
+
+	const handleSelectChange = (value) => {
+		setForm((oldForm) => ({ ...oldForm, tip: value }));
+		console.log(form);
+	};
+
+	const onSubmit = () => {
+		if (form.tip == "") {
+			alert("Odaberite vrstu terena.");
+			return;
+		}
+		const data = JSON.stringify(form);
+		console.log(data);
+
+		const options = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: data,
+		};
+
+		axios({
+			url: "/api/tereni/",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: data,
+		})
+			.then((res) => {
+				setOpenRes(true);
+				setOpenTerenInp(false);
+			})
+			.catch((err) => {});
+	};
 
 	if (!userInfo) return <p>Loading...</p>;
 
@@ -69,27 +112,90 @@ export default function CourtPreview() {
 				<div className="flex">
 					<h1 className="text-left m-10">Moji tereni</h1>
 				</div>
-				<Button className="h-fit text-white ml-10">Dodaj novi</Button>
+
+				<Dialog open={openTerenInp} onOpenChange={setOpenTerenInp}>
+					<DialogTrigger asChild>
+						<Button className="h-fit text-white ml-10">
+							Dodaj novi teren
+						</Button>
+					</DialogTrigger>
+					<DialogContent className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>Dodaj novi teren</DialogTitle>
+							<DialogDescription>
+								Upiši podatke o terenu kojeg želiš dodati.
+							</DialogDescription>
+						</DialogHeader>
+						<form onSubmit={(e) => e.preventDefault()}>
+							<div className="grid gap-4 py-4">
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="name" className="text-right">
+										Naziv
+									</Label>
+									<Input
+										id="naziv"
+										name="naziv"
+										value={form.naziv}
+										onChange={onChange}
+										className="col-span-3"
+									/>
+								</div>
+
+								<Select onValueChange={handleSelectChange}>
+									<SelectTrigger id="framework">
+										<SelectValue placeholder="Tip terena" />
+									</SelectTrigger>
+									<SelectContent position="popper">
+										<SelectItem value="vanjski">vanjski</SelectItem>
+										<SelectItem value="unutarnji">
+											unutarnji
+										</SelectItem>
+									</SelectContent>
+								</Select>
+							</div>
+						</form>
+						<DialogFooter>
+							<Button
+								type="submit"
+								onClick={() => onSubmit()}
+								className="h-fit text-white ml-10"
+							>
+								Dodaj
+							</Button>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
+
+				<Dialog open={openRes} onOpenChange={setOpenRes}>
+					<DialogContent className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>Dodano!</DialogTitle>
+						</DialogHeader>
+					</DialogContent>
+				</Dialog>
 
 				<div className="flex h-fit flex-wrap">
-					{courts.map((court) => (
-						<Card className="w-[350px] m-8">
-							<CardHeader>
-								<CardTitle>{court.name}</CardTitle>
-								<CardDescription>{court.type}</CardDescription>
-								<CardDescription>{court.location}</CardDescription>
-							</CardHeader>
-							<CardContent>
-								<image></image>
-							</CardContent>
-							<CardFooter className="flex justify-between">
-								{/* on click open court details */}
-								<Button variant="outline" className="text-white">
-									Više
-								</Button>
-							</CardFooter>
-						</Card>
-					))}
+					{courts
+						// currently using filter, should have unique endpoint in the future
+						.filter((court) => court.vlasnikTeren.email == userInfo.email)
+						.map((court) => (
+							<Card key={court.idteren} className="w-[350px] m-8">
+								<CardHeader>
+									<CardTitle>{court.nazivTeren}</CardTitle>
+									<CardDescription>{court.tip}</CardDescription>
+								</CardHeader>
+								<CardContent>
+									<p> > {court.tipTeren}</p>
+									<p>vlasnik: {court.vlasnikTeren.email}</p>
+								</CardContent>
+								<CardFooter className="flex justify-between">
+									{/* on click open court details */}
+									<Button variant="outline" className="text-white">
+										Rezerviraj
+									</Button>
+								</CardFooter>
+							</Card>
+						))}
 				</div>
 			</div>
 		</>

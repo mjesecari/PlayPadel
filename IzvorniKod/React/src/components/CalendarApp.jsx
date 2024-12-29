06d36237fa7232b7e753ea3774ce2ff2ac8a3ce8
LorenaIvanisevic <lorena.ivanisevic@fer.unizg.hr>
2024-12-29 18:35:40 +0100

    changed navbar, added button that redirects to home, added content on main page so it isnt empty

--- IzvorniKod/React/src/components/CalendarApp.jsx
+++ IzvorniKod/React/src/components/CalendarApp.jsx
@@ -1,14 +1,4 @@
 import { useState, useEffect } from "react";
-import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
-import {
-	createViewDay,
-	createViewMonthAgenda,
-	createViewMonthGrid,
-	createViewWeek,
-} from "@schedule-x/calendar";
-import { createEventsServicePlugin } from "@schedule-x/events-service";
-
-import "@schedule-x/theme-default/dist/index.css";
 
 function CalendarApp({ eventsProp }) {
 	console.log(eventsProp);
